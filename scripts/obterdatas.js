ec39c828db4f67a9fc277f02310c44b0b09ef0fa
Miguel Enrique <miguelenriquefr96@gmail.com>
2026-07-04 09:27:37 -0300

    ajuste JS formato data hora

--- scripts/obterdatas.js
+++ scripts/obterdatas.js
@@ -5,5 +5,18 @@ if (anoAtualElement) {
 
 const ultimaModificacaoElement = document.getElementById('ultimaModificacao');
 if (ultimaModificacaoElement) {
-    ultimaModificacaoElement.textContent = `Última modificação: ${document.lastModified}`;
+
+    const dataModificacao = new Date(document.lastModified);
+
+
+    const dataFormatada = dataModificacao.toLocaleString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+    ultimaModificacaoElement.textContent = `Última modificação: ${dataFormatada}`;
 }
